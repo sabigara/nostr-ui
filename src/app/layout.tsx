0489c6f927e44ef1ea83f183683f4387b5fa14fb
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-02-13 01:32:13 +0900

    Enable persister

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+
 import "@camome/system/dist/theme.css";
 import "./globals.scss";
 
@@ -9,8 +12,18 @@ const queryClient = new QueryClient({
     queries: {
       retry: false,
       refetchOnWindowFocus: false,
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
     },
   },
+});
+
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
 });
 
 export default function RootLayout({
